039628b659092b8d85e73b24f52da161fc51a9c8
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2020-08-31 19:29:57 -0300

    agrego html basico

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+
+form.addEventListener('submit',(e)=>{
+    
+    e.preventDefault()
+})
